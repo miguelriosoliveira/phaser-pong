f334d7eb660786bbb7bb47060d83449d30865ec0
Miguel Rios <miguelriosoliveira@gmail.com>
2017-07-11 00:01:38 -0300

    Changed my mind, learning everything from scratch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const babel = require('gulp-babel');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const babel = require("gulp-babel");
 
 // Static server
-gulp.task('browser-sync', function () {
+gulp.task("browser-sync", function () {
 	browserSync.init({
 		server: {
 			baseDir: "./dist"
@@ -13,20 +13,29 @@ gulp.task('browser-sync', function () {
 	gulp.watch("dist/js/*.js").on("change", browserSync.reload);
 });
 
-gulp.task('babel', () => {
-	return gulp.src('js/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('dist/js'));
+gulp.task("babel", function () {
+    return gulp.src("js/*.js")
+        .pipe(babel({
+            presets: ["es2015"]
+        }))
+        .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('copy', function () {
-	gulp.src('assets/*').pipe(gulp.dest('dist/assets/'));
-	return gulp.src('*.html').pipe(gulp.dest('dist/'));
+gulp.task("html", function () {
+    return gulp.src("index.html").pipe(gulp.dest("dist/"));
 });
 
-gulp.task('default', ['browser-sync', 'babel', 'copy'], function () {
-	gulp.watch("html/*.html", ['copy']);
-	gulp.watch("js/*.js", ['babel']);
+gulp.task("assets", function () {
+    return gulp.src("assets/*").pipe(gulp.dest("dist/assets/"));
+});
+
+gulp.task("libs", function () {
+    return gulp.src("js/libs/**/*.js").pipe(gulp.dest("dist/js/libs/"));
+});
+
+gulp.task("default", ["browser-sync", "babel", "html", "libs", "assets"], function () {
+    gulp.watch("js/libs/*", ["libs"]);
+    gulp.watch("assets/*", ["assets"]);
+    gulp.watch("index.html", ["html"]);
+    gulp.watch("js/*.js", ["babel"]);
 });
